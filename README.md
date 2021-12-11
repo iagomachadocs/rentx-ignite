2ce2990e234a3e0b2e57d1ed6fd39fe2a82590dd
Iago Machado <iagomachadocs@gmail.com>
2021-12-10 22:24:22 -0300

    Add create cars use case

--- README.md
+++ README.md
@@ -8,11 +8,9 @@ Aplicação de uma locadora de carros desenvolvida durante o ignite
 
 **RF**
 Deve ser possível cadastrar um novo carro.
-Deve ser possível listar todas as categorias.
 
 **RN**
 Não deve ser possível cadastrar um carro com uma placa já existente.
-Não deve ser possível alterar a placa de um carro já cadastrado.
 O carro deve ser cadastrado como disponível por padrão.
 O usuário responsável pelo cadastro deve ser um usuário administrador.
 
---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,66 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
+
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepository: CarsRepositoryInMemory;
+
+describe("Create Car", () => {
+  beforeEach(() => {
+    carsRepository = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepository);
+  });
+
+  it("should be able to create a new car", async () => {
+    const car = await createCarUseCase.execute({
+      name: "Car Name",
+      description: "Car Description",
+      daily_rate: 100,
+      license_plate: "IAG-0123",
+      fine_amount: 50,
+      brand: "Car Brand",
+      category_id: "category",
+    });
+
+    expect(car).toHaveProperty("id");
+  });
+
+  it("should not be able to create a car with an existing license plate", () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: "Gol bola",
+        description: "Car Description",
+        daily_rate: 100,
+        license_plate: "IAG-0123",
+        fine_amount: 50,
+        brand: "Car Brand",
+        category_id: "category",
+      });
+
+      await createCarUseCase.execute({
+        name: "Celta",
+        description: "Car Description",
+        daily_rate: 100,
+        license_plate: "IAG-0123",
+        fine_amount: 50,
+        brand: "Car Brand",
+        category_id: "category",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should be able to create a car with available true by default", async () => {
+    const car = await createCarUseCase.execute({
+      name: "Car Name",
+      description: "Car Description",
+      daily_rate: 100,
+      license_plate: "ABC-0123",
+      fine_amount: 50,
+      brand: "Car Brand",
+      category_id: "category",
+    });
+
+    expect(car.available).toBe(true);
+  });
+});
